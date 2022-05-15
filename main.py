d8a655e5edb8e18355d6267ea8fe75ba8e5ee9da
gcyburt <gcyburt@gmail.com>
2022-05-15 20:34:43 +0200

    refact

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import argparse, sys
 import os
 from datetime import datetime
 
-
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--duration_in_minutes')
@@ -18,18 +17,18 @@ busyTime = dict()
 
 for calendar in os.listdir(calendarsDirectory):
     f = open(f"{calendarsDirectory}/{calendar}", 'r')
-    name=os.path.basename(f"{calendarsDirectory}/{calendar}").split('.')[0]
+    name = os.path.basename(f"{calendarsDirectory}/{calendar}").split('.')[0]
     content = f.readlines()
     for lines in content:
-        splitted=lines.partition(' - ')
-        if(splitted[2]==''):
-            date= datetime.strptime(splitted[0], '%Y-%m-%d')
-            busyTime[name]=((date,datetime.strptime(splitted[0]+ '23:59:59', '%Y-%m-%d %H:%M:%S')))
+        splitted = lines.partition(' - ')
+        if (splitted[2] == ''):
+            date = datetime.strptime(splitted[0], '%Y-%m-%d')
+            busyTime[name] = ((date, datetime.strptime(splitted[0] + '23:59:59', '%Y-%m-%d %H:%M:%S')))
 
         else:
-            date1= datetime.strptime(splitted[0], '%Y-%m-%d %H:%M:%S')
-            date2= datetime.strptime(splitted[2], '%Y-%m-%d %H:%M:%S')
-            busyTime[name]=((date1,date2))
+            date1 = datetime.strptime(splitted[0], '%Y-%m-%d %H:%M:%S')
+            date2 = datetime.strptime(splitted[2], '%Y-%m-%d %H:%M:%S')
+            busyTime[name] = ((date1, date2))
     f.close()
 
 print(busyTime)
