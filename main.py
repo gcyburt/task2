d8f705320180e99b4fb60e5043b71411d52e81d4
gcyburt <gcyburt@gmail.com>
2022-05-15 19:26:50 +0200

    z

--- main.py
+++ main.py
@@ -3,23 +3,22 @@ import datefinder
 import os
 
 
-def main():
-    parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser()
 
-    parser.add_argument('--duration_in_minutes')
-    parser.add_argument('--minimum_people')
-    parser.add_argument('--calendars')
-    args = parser.parse_args()
+parser.add_argument('--duration_in_minutes')
+parser.add_argument('--minimum_people')
+parser.add_argument('--calendars')
+args = parser.parse_args()
 
-    duration = args.duration_in_minutes
-    minimumPeople = args.minimum_people
-    calendarsDirectory = args.calendars
+duration = args.duration_in_minutes
+minimumPeople = args.minimum_people
+calendarsDirectory = args.calendars
 
-    busyTime = dict()
+busyTime = dict()
 
-    for calendar in os.listdir(calendarsDirectory):
-        f = open(calendar, 'r')
-        content = f.read()
-        for lines in content:
-            busyTime[calendar].append(list(datefinder.find_dates(lines)))
-        f.close()
+for calendar in os.listdir(calendarsDirectory):
+    f = open(calendar, 'r')
+    content = f.read()
+    for lines in content:
+        busyTime[calendar].append(list(datefinder.find_dates(lines)))
+    f.close()
